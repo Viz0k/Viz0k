e8ff00b11aef1d4657f99017a140eed676bb678b
Viz0k <25bscarborough@pangbourne.com>
2024-10-18 10:45:20 +0100

    UPPERCUT v4.2 latest bug

--- UpperCut/Playerscopy.py
+++ UpperCut/Playerscopy.py
@@ -13,18 +13,24 @@ class Player(pygame.sprite.Sprite):
         self.pos = pygame.math.Vector2(self.rect.topleft)  
         self.speed = 300
         self.colour = colour
+        self.punch = False
+
+        self.current_ani = []
 
         self.Ucolour = self.colour[0].upper()
         self.CW1 = pygame.image.load(join('UpperCut', 'graphics', 'players', self.Ucolour + 'CW1.png')).convert_alpha()
         self.CW2 = pygame.image.load(join('UpperCut', 'graphics', 'players', self.Ucolour + 'CW2.png')).convert_alpha()
         self.CW = [self.CW1, self.CW2]
         self.CW_index = 0
-        self.walk = self.CW[self.CW_index]
+
+        self.CJ1 = pygame.image.load(join('UpperCut', 'graphics', 'players', self.Ucolour + 'CJ1.png')).convert_alpha()
+        self.CJ2 = pygame.image.load(join('UpperCut', 'graphics', 'players', self.Ucolour + 'CJ2.png')).convert_alpha()
+        self.CJ = [self.CJ1, self.CJ2, self.stand]
+        self.CJ_index = 0
 
     def update(self, dt):
         self.boundaries()
         self.input()
-        # self.animation()
 
         self.pos.x += self.direction.x * self.speed * dt
         self.rect.x = self.pos.x
@@ -46,10 +52,16 @@ class Player(pygame.sprite.Sprite):
         if self.colour == "red":
             if keys[pygame.K_a]:
                 self.direction.x = -1
-                self.animation()
+                self.walk()
             elif keys[pygame.K_d]:
                 self.direction.x = 1
-                self.animation()
+                self.walk()
+            elif keys[pygame.K_e]:
+                self.punch = True
+                self.jab()
+                # self.jab()
+                # if self.CJ_index >= len(self.CJ):
+                #     self.CJ_index = 0
             else:
                 self.direction.x = 0
                 self.image = self.stand
@@ -58,16 +70,35 @@ class Player(pygame.sprite.Sprite):
             x_speed = round(pygame.joystick.Joystick(0).get_axis(0))
             if x_speed == 1:
                 self.direction.x = 1
-                self.animation()
+                self.walk()
             elif x_speed == -1:
                 self.direction.x = -1
-                self.animation()
+                self.walk()
             else:
                 self.direction.x = 0
                 self.image = self.stand
 
-    def animation(self):
+    def walk(self):
         self.CW_index = self.CW_index + 0.03
         if self.CW_index >= len(self.CW):
             self.CW_index = 0
         self.image = self.CW[int(self.CW_index)]
+    
+    def jab(self):
+        # for item in self.CJ:
+        #     self.current_ani.append(item)
+        # for i in self.current_ani:
+        #     self.image = self.current_ani
+
+        # while self.punch == True:
+        #     self.CJ_index = self.CJ_index + 0.03
+        #     if self.CW_index < len(self.CW):
+        #         self.image = self.CJ[int(self.CJ_index)]
+        #     elif self.CW_index >= len(self.CW):
+        #         self.punch = False
+
+        if self.punch == True:
+            self.image = self.CJ1
+            self.image = self.CJ2
+            # self.image = self.stand
+            self.punch = False
--- UpperCut/test.py
+++ UpperCut/test.py
@@ -1,3 +1,31 @@
-text = 'test string'
-uppertext = text[0].upper()
-print(uppertext)
+import pygame
+
+# Initialize pygame
+pygame.init()
+screen = pygame.display.set_mode((400, 300))
+
+running = True
+key_pressed = False  # To track the first press
+
+while running:
+    # Handle events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Get the state of all keys
+    keys = pygame.key.get_pressed()
+
+    # Detect single press of the SPACE key
+    if keys[pygame.K_SPACE] and not key_pressed:
+        print("Spacebar pressed!")
+        key_pressed = True  # Mark that the key has been pressed
+
+    # Reset when the key is released
+    if not keys[pygame.K_SPACE]:
+        key_pressed = False
+
+    # Update the display (optional for this example)
+    pygame.display.flip()
+
+pygame.quit()
